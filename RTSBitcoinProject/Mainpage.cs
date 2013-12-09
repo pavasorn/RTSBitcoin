f2625935748fdaa52c3c0736fc61ccc2e54dce53
pavasorn <pavasorn.n@gmail.com>
2013-12-09 17:44:29 +0100

    changesssssss

--- RTSBitcoinProject/Mainpage.cs
+++ RTSBitcoinProject/Mainpage.cs
@@ -24,7 +24,7 @@ namespace RTSBitcoinProject
 
         private void buyatButton_Click(object sender, EventArgs e)
         {
-            ///// test changes nnnnnnnnn
+            // jimmy choo
         }
     }
 }
